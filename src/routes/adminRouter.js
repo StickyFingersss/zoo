aa24402a7a2341b7f61e803aeea09fd3a0b16239
Ayur <ayurrr98@gmail.com>
2023-07-19 23:01:15 +0800

    Done

---
+++ src/routes/adminRouter.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const route = express.Router();
+
+const render = require("../lib/render");
+const AdminButton = require("../views/AdminButton");
+const ChangeTarif = require("../views/ChangeTarif");
+
+const { Rate } = require("../../db/models");
+
+route.get("/", (req, res) => {
+  render(AdminButton, {}, res);
+});
+route.get("/Change", async (req, res) => {
+  const rate = await Rate.findAll({ raw: true });
+
+  render(ChangeTarif, { rate }, res);
+});
+
+route.put("/Change", async (req, res) => {
+  const { oldBud, oldWeek, youngBud, youngWeek } = req.body;
+  console.log(req.body);
+  console.log("------------------>");
+  await Rate.update({ price: Number(oldBud) }, { where: { id: 3 } });
+  await Rate.update({ price: Number(oldWeek) }, { where: { id: 4 } });
+  await Rate.update({ price: Number(youngBud) }, { where: { id: 1 } });
+  await Rate.update({ price: Number(youngWeek) }, { where: { id: 2 } });
+});
+
+module.exports = route;
