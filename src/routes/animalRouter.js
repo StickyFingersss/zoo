20077536b501dc6bf71bfe39e7892e5875f3775c
FliteVPN <nepopov70@gmail.com>
2023-07-19 17:42:38 +0500

    client animals done

--- src/routes/animalRouter.js
+++ src/routes/animalRouter.js
@@ -3,10 +3,14 @@ const express = require("express");
 const route = express.Router();
 
 const render = require("../lib/render");
-const Animal = require("../views/Animal");
+const AnimalView = require("../views/Animal");
+const { Animal, Image } = require("../../db/models");
 
-route.get("/:id", (req, res) => {
-  render(Animal, {}, res);
+route.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const animal = await Animal.findOne({ where: {id}});
+  const images = await Image.findAll({ where: { animal_id: req.params.id }});
+  render(AnimalView, { animal, images }, res);
 });
 
 module.exports = route;
