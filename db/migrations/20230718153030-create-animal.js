a9a5f1e24081f25834e0ca69bdca740c0472892a
StickyFingersss <dynko.valerii@gmail.com>
2023-07-19 18:06:21 +0600

    animals done

--- db/migrations/20230718153030-create-animal.js
+++ db/migrations/20230718153030-create-animal.js
@@ -1,34 +1,34 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Animals', {
+    await queryInterface.createTable("Animals", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(1000),
       },
       species: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Animals');
-  }
+    await queryInterface.dropTable("Animals");
+  },
 };
--- src/views/Animals.jsx
+++ src/views/Animals.jsx
@@ -1,26 +1,72 @@
 const React = require("react");
 const Layout = require("./Layout");
 
-module.exports = function Login() {
+module.exports = function Animals({ login, animalDataDb, imagelDataDb }) {
   return (
     <Layout>
       <ul className="animals-list">
-        <li className="animal">
-          <a href={`/animal/1`}>
-            <img src="/images/enot.png" alt="" className="animal-img" />
-          </a>
-          <div className="animal-info">
-            <h3 className="animal-name">Animal Name</h3>
-          </div>
-          <div className="buttons-edit-animals">
-            <button>Изменить</button>
-            <button>Trash</button>
-          </div>
-        </li>
+        {login
+          ? animalDataDb.map((animal) => {
+              const imageLink = imagelDataDb.find(
+                (element) => element.animal_id === animal.id
+              ).link;
+
+              return (
+                <li className="animal" key={animal.id}>
+                  <a href={`/animal/${animal.id}`}>
+                    <img
+                      src={`${
+                        imagelDataDb.find(
+                          (element) => element.animal_id === animal.id
+                        ).link
+                      }`}
+                      alt=""
+                      className="animal-img"
+                    />
+                  </a>
+                  <div className="animal-info">
+                    <h3 className="animal-name">{animal.name}</h3>
+                  </div>
+                  <div className="buttons-edit-animals">
+                    <button>Изменить</button>
+                    <button>Trash</button>
+                  </div>
+                </li>
+              );
+            })
+          : animalDataDb.map((animal) => {
+              const imageLink = imagelDataDb.find(
+                (element) => element.animal_id === animal.id
+              ).link;
+
+              return (
+                <li className="animal" key={animal.id}>
+                  <a href={`/animal/${animal.id}`}>
+                    <img
+                      src={`${
+                        imagelDataDb.find(
+                          (element) => element.animal_id === animal.id
+                        ).link
+                      }`}
+                      alt=""
+                      className="animal-img"
+                    />
+                  </a>
+                  <div className="animal-info">
+                    <h3 className="animal-name">{animal.name}</h3>
+                  </div>
+                </li>
+              );
+            })}
       </ul>
-      <div className="buttons-add-animals">
-        <button>Добавить новое животное</button>
-      </div>
+
+      {login ? (
+        <div className="buttons-add-animals">
+          <button>Добавить новое животное</button>
+        </div>
+      ) : (
+        <span></span>
+      )}
     </Layout>
   );
 };
