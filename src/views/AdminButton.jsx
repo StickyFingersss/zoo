f751bc004dad4e29606d7d447a55d93fa34fa714
FliteVPN <nepopov70@gmail.com>
2023-07-20 15:40:14 +0500

    fixed

--- src/views/AdminButton.jsx
+++ src/views/AdminButton.jsx
@@ -4,14 +4,14 @@ const Layout = require("./Layout");
 module.exports = function Buttons() {
   return (
     <Layout>
-      <a href="/admin">
+      <a href="/">
         <button>Назад в главное меню</button>
       </a>
       <div class="d-grid gap-2 col-6 mx-auto" style={{ marginTop: "300px" }}>
         <a class="btn btn-primary" href="/admin/Change">
           Изменить тарифы
         </a>
-        <a class="btn btn-primary" type="button">
+        <a class="btn btn-primary" href="/animals" type="button">
           Изменить животных / Добавить животных
         </a>
       </div>
